ee16cf001c5bb658990461eeafa302e727b1cc32
dan sinclair <dj2@everburning.com>
2011-02-10 23:49:02 -0500

    fiber is gone, no thread local. boo

--- examples/stream.rb
+++ examples/stream.rb
@@ -22,15 +22,15 @@ class Stream < Goliath::API
   def response(env)
     i = 0
     pt = EM.add_periodic_timer(1) do
-      stream_send("#{i} ")
+      env.stream_send("#{i} ")
       i += 1
     end
 
     EM.add_timer(10) do
       pt.cancel
 
-      stream_send("!! BOOM !!\n")
-      stream_close
+      env.stream_send("!! BOOM !!\n")
+      env.stream_close
     end
 
     [200, {}, Goliath::Response::STREAMING]
