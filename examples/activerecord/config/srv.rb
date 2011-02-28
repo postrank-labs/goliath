59d25cad9db25af1891866093a4d1da77e25cccc
dan sinclair <dj2@everburning.com>
2011-02-27 23:24:41 -0500

    add activerecord example

---
+++ examples/activerecord/config/srv.rb
@@ -0,0 +1,7 @@
+require 'mysql2'
+
+ActiveRecord::Base.establish_connection(:adapter  => 'em_mysql2',
+                                        :database => 'goliath_test',
+                                        :username => 'goliath',
+                                        :password => 'goliath',
+                                        :host     => 'localhost')
---
+++ examples/activerecord/srv.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+# gem install activerecord
+# gem install mysql2
+
+# create database goliath_test
+# create user 'goliath'@'localhost' identified by 'goliath'
+# grant all on goliath_test.* to 'goliath'@'localhost'
+# create table users (id int not null auto_increment primary key, name varchar(255), email varchar(255));
+# insert into users (name, email) values ('dan', 'dj2@everyburning.com'), ('Ilya', 'ilya@igvita.com');
+
+$: << "../../lib" << "./lib"
+
+require 'goliath'
+require 'active_record'
+require 'yajl'
+
+class User < ActiveRecord::Base
+end
+
+class Srv < Goliath::API
+  use ::Rack::Reloader, 0 if Goliath.dev?
+
+  use Goliath::Rack::Params
+  use Goliath::Rack::DefaultMimeType
+  use Goliath::Rack::Formatters::JSON
+  use Goliath::Rack::Render
+  use Goliath::Rack::ValidationError
+
+  use Goliath::Rack::Validation::RequiredParam, {:key => 'id', :type => 'ID'}
+  use Goliath::Rack::Validation::NumericRange, {:key => 'id', :min => 1}
+
+  def response(env)
+    User.find_by_sql("SELECT SLEEP(10)")
+    [200, {}, User.find(params['id'])]
+  end
+end
