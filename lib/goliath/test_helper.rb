d6f544b8cb61759f87c5536a32481237af92c94e
dan sinclair <dj2@everburning.com>
2011-03-04 22:30:06 -0500

    add docs to test helper

--- lib/goliath/test_helper.rb
+++ lib/goliath/test_helper.rb
@@ -5,7 +5,28 @@ require 'goliath/server'
 require 'rack'
 
 module Goliath
+  # Methods to help with testing Goliath APIs
+  #
+  # @example
+  #   describe Echo do
+  #     include Goliath::TestHelper
+  #
+  #     let(:err) { Proc.new { fail "API request failed" } }
+  #     it 'returns the echo param' do
+  #       with_api(Echo) do
+  #         get_request({:query => {:echo => 'test'}}, err) do |c|
+  #           b = Yajl::Parser.parse(c.response)
+  #           b['response'].should == 'test'
+  #         end
+  #       end
+  #     end
+  #   end
+  #
   module TestHelper
+    # Builds the rack middleware chain for the given API
+    #
+    # @param klass [Class] The API class to build the middlewares for
+    # @return [Object] The Rack middleware chain
     def build_app(klass)
       ::Rack::Builder.new do
         klass.middlewares.each do |mw|
@@ -15,6 +36,11 @@ module Goliath
       end
     end
 
+    # Launches an instance of a given API server. The server
+    # will launch on the default settings of localhost port 9000.
+    #
+    # @param api [Class] The API class to launch
+    # @return [Nil]
     def server(api)
       s = Goliath::Server.new
       s.logger = mock('log').as_null_object
@@ -23,10 +49,19 @@ module Goliath
       s.start
     end
 
+    # Stops the launched API
+    #
+    # @return [Nil]
     def stop
       EM.stop
     end
 
+    # Wrapper for launching API and executing given code block. This
+    # will start the EventMachine reactor running.
+    #
+    # @param api [Class] The API class to launch
+    # @param blk [Proc] The code to execute after the server is launched.
+    # @note This will not return until stop is called.
     def with_api(api, &blk)
       EM.synchrony do
         server(api)
@@ -34,6 +69,15 @@ module Goliath
       end
     end
 
+    # Helper method to setup common callbacks for various request methods.
+    # The given err and callback handlers will be attached and a callback
+    # to stop the reactor will be added.
+    #
+    # @param req [EM::HttpRequest] The HTTP request to augment
+    # @param errback [Proc] An error handler to attach
+    # @param blk [Proc] The callback handler to attach
+    # @return [Nil]
+    # @api private
     def hookup_request_callbacks(req, errback, &blk)
       req.callback &blk
       req.callback { stop }
@@ -42,11 +86,21 @@ module Goliath
       req.errback { stop }
     end
 
+    # Make a GET request the currently launched API.
+    #
+    # @param request_data [Hash] Any data to pass to the GET request.
+    # @param errback [Proc] An error handler to attach
+    # @param blk [Proc] The callback block to execute
     def get_request(request_data = {}, errback = nil, &blk)
       req = EM::HttpRequest.new('http://localhost:9000').get(request_data)
       hookup_request_callbacks(req, errback, &blk)
     end
 
+    # Make a POST request the currently launched API.
+    #
+    # @param request_data [Hash] Any data to pass to the POST request.
+    # @param errback [Proc] An error handler to attach
+    # @param blk [Proc] The callback block to execute
     def post_request(request_data = {}, errback = nil, &blk)
       req = EM::HttpRequest.new('http://localhost:9000').post(request_data)
       hookup_request_callbacks(req, errback, &blk)
