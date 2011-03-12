9342ab0183ee7bd0c8add1993447008cc5e66d08
dan sinclair <dj2@everburning.com>
2011-03-11 21:26:38 -0500

    better example; chat server

---
+++ examples/config/websocket.rb
@@ -0,0 +1,1 @@
+config['channel'] = EM::Channel.new
--- examples/websocket.rb
+++ examples/websocket.rb
@@ -15,16 +15,17 @@ class Websocket < Goliath::WebSocket
 
   def on_open(env)
     env.logger.info "WS OPEN"
+    env['subscription'] = env.channel.subscribe { |m| env.stream_send(m) }
   end
 
   def on_message(env, msg)
     env.logger.info "WS MESSAGE: #{msg}"
-
-    env.stream_send("Goliath welcomes you #{msg}")
+    env.channel << msg
   end
 
   def on_close(env)
     env.logger.info "WS CLOSED"
+    env.channel.unsubscribe(env['subscription'])
   end
 
   def on_error(env, error)
