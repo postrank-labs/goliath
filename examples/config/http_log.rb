cdafe3db820c3e50f3fcb9623d3c98e50bcdf8b2
dan sinclair <dj2@everburning.com>
2011-03-20 22:47:49 -0400

    only connect ot mongo in dev mode

--- examples/config/http_log.rb
+++ examples/config/http_log.rb
@@ -1,7 +1,9 @@
 config['forwarder'] = 'http://localhost:8080'
 
-config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
-  # Need to deal with this just never connecting ... ?
-  conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
-  conn.db('http_log').collection('aggregators')
+environment(:development) do
+  config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
+    # Need to deal with this just never connecting ... ?
+    conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
+    conn.db('http_log').collection('aggregators')
+  end
 end
