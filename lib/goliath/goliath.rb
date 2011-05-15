b7e4410d81df6a15460f10569455c60b63521207
Greg Weber <greg@gregweber.info>
2011-05-15 12:16:05 -0700

    Goliath.env should throw an exception if unset
    see https://github.com/postrank-labs/goliath/issues/18

--- lib/goliath/goliath.rb
+++ lib/goliath/goliath.rb
@@ -6,19 +6,31 @@ require 'async_rack'
 module Goliath
   module_function
 
-  @env = :development
+  ENVIRONMENTS = [:development, :production, :test]
 
   # Retrieves the current goliath environment
   #
   # @return [String] the current environment
   def env
-    @env
+    @env or fail "environment has not been set"
+  end
+
+  # Sets the current goliath environment
+  #
+  # @param [Symbol] env the environment symbol of [development|production|test]
+  def env= e
+    es = e.to_sym
+    if ENVIRONMENTS.include? es
+      @env = es
+    else
+      fail "did not recognize environment: #{e}, expected one of: #{ENVIRONMENTS.join(', ')}"
+    end
   end
 
   # Sets the current goliath environment
   #
   # @param [String] env the environment string of [dev|prod|test]
-  def env=(env)
+  def short_env=(env)
     case(env.to_s)
     when 'dev'  then @env = :development
     when 'prod' then @env = :production
@@ -30,20 +42,20 @@ module Goliath
   #
   # @return [Boolean] true if current environemnt is production, false otherwise
   def prod?
-    @env == :production
+    env == :production
   end
 
   # Determines if we are in the development environment
   #
   # @return [Boolean] true if current environemnt is development, false otherwise
   def dev?
-    @env == :development
+    env == :development
   end
 
   # Determines if we are in the test environment
   #
   # @return [Boolean] true if current environemnt is test, false otherwise
   def test?
-    @env == :test
+    env == :test
   end
 end
