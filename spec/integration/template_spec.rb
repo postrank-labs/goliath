01feeb850e38f92de471e0816e395770118e2eb2
Frederick Ros <frederick.ros@gmail.com>
2011-08-08 23:26:17 +0200

    rake test now works again
    The environment was not correctly set before running
    the 'template' spec. Now it is forced to 'test' before
    actually running the specs

--- spec/integration/template_spec.rb
+++ spec/integration/template_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require File.join(File.dirname(__FILE__), '../../', 'examples/template')
 
+
 describe Template do
 
   def config_file
@@ -9,7 +10,10 @@ describe Template do
 
   let(:api_options) { { :config => config_file } }
 
+  before(:all) {     Goliath.env = :test }
+  
   it 'renders haml template with default haml layout' do
+    
     with_api(Template, api_options) do
       get_request do |c|
         c.response.should =~ %r{<li><a href="/joke">Tell me a joke</a></li>}
