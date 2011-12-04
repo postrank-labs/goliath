e37b1f99949160098eae43fb8ab9b8e1370378e2
Julien Ammous <schmurfy@gmail.com>
2011-12-04 19:10:41 +0100

    added test for middle wares inheritance

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -9,6 +9,25 @@ describe Goliath::API do
     it "doesn't change after multi calls" do
       2.times { Goliath::API.should have(2).middlewares }
     end
+    
+    it "should include all middlewares from superclasses" do
+      c1 = Class.new(Goliath::API) do
+        use Goliath::Rack::Params
+      end
+      
+      c2 = Class.new(c1) do
+        use Goliath::Rack::DefaultMimeType
+      end
+      
+      base_middlewares = DummyApi.middlewares
+      
+      middlewares = c2.middlewares - base_middlewares
+      
+      middlewares.size.should == 2
+      middlewares[0][0].should == Goliath::Rack::DefaultMimeType
+      middlewares[1][0].should == Goliath::Rack::Params
+    end
+    
   end
 
   describe ".maps?" do
